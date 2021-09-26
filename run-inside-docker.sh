dc27d514ef5f1e209cd07f104c60921290693e64
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-09-26 20:39:57 +0200

    bump asciidoctor to 1.11.0; change master-branch to main

--- run-inside-docker.sh
+++ run-inside-docker.sh
@@ -6,12 +6,12 @@ echo "OLD->${OLD}"
 docker run --rm \
            -v ${PWD}:/documents \
            --env-file ./.env \
-           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/publish-gh-pages-inside-docker.sh"
+           asciidoctor/docker-asciidoctor:1.11.0  /bin/bash -c "/documents/publish-gh-pages-inside-docker.sh"
            #asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/build-html-docker-inside-docker.sh"
 cd $PWD/$BUILD_DIR
 CURRENT_FOLDER=${PWD}
 echo "current folder->${CURRENT_FOLDER}"
-git push --force origin master:gh-pages
+git push --force origin main:gh-pages
 echo gh-pages pushed to github ...
 cd $OLD
 rm -rf $BUILD_DIR
