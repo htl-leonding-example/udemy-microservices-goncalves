a32a657eaa33e54c680871e328f8d1e209f1f5bd
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-10-14 23:55:48 +0200

    add REST client to book microservice

--- labs/vintage-store/rest-book/src/main/java/at/htl/microservices/book/Book.java
+++ labs/vintage-store/rest-book/src/main/java/at/htl/microservices/book/Book.java
@@ -1,14 +1,26 @@
 package at.htl.microservices.book;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbProperty;
 import java.time.Instant;
 
+@Schema(description = "This is a book")
 public class Book {
 
+    @JsonbProperty("isbn_13")
+    @Schema(required = true)
     public String isbn13;
+    @Schema(required = true)
     public String title;
     public String author;
+    @JsonbProperty("year_of_publication")
     public int yearOfPublication;
     public String genre;
+    @JsonbProperty("creation_date")
+    @JsonbDateFormat("yyyy-MM-dd")
+    @Schema(implementation = String.class, format = "date")
     public Instant creationTime;
 
     @Override
--- labs/vintage-store/rest-book/src/main/java/at/htl/microservices/book/BookResource.java
+++ labs/vintage-store/rest-book/src/main/java/at/htl/microservices/book/BookResource.java
@@ -1,5 +1,8 @@
 package at.htl.microservices.book;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -9,14 +12,22 @@ import javax.ws.rs.core.Response;
 import java.time.Instant;
 
 @Path("/api/books")
+@Tag(name="Book REST endpoint")
 public class BookResource {
 
     @Inject
     Logger logger;
 
+    @RestClient
+    NumberProxy proxy;
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary = "Creates a book",
+            description = "Creates a book with ISBN number"
+    )
     public Response createABook(
             @FormParam("title") String title,
             @FormParam("author") String author,
@@ -24,7 +35,7 @@ public class BookResource {
             @FormParam("genre") String genre
     ) {
         Book book = new Book();
-        book.isbn13 = "13-we will get it from the number microservice";
+        book.isbn13 = proxy.generateIsbnNumbers().isbn13;
         book.title = title;
         book.author = author;
         book.yearOfPublication = yearOfPublication;
